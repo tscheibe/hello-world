e56248e4d172e7ca2921f3dda8e3602df050b5ce
tscheibe <tim.scheibe@pnnl.gov>
2026-06-05 11:40:15 -0700

    Changed name to Tim

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const message = document.getElementById('message');
 
 button.addEventListener('click', () => {
   const timeOfDay = new Date().getHours() < 12 ? 'morning' : 'afternoon';
-  message.textContent = `Good ${timeOfDay} Sunshine! You are ready to learn GitHub and Codex.`; 
+  message.textContent = `Good ${timeOfDay} Tim! You are ready to learn GitHub and Codex.`; 
 });
